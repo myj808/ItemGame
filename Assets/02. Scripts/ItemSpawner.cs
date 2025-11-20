a4b168d54666c2b532e7f08852b4c60f60bc2ba2
myj808 <jylove100412@gmail.com>
2025-11-20 15:05:30 +0900

    필수 기능 요소 추가 커밋

--- Assets/02. Scripts/ItemSpawner.cs
+++ Assets/02. Scripts/ItemSpawner.cs
@@ -1,5 +1,6 @@
 
 using UnityEngine;
+using System.Collections.Generic;
 
 public class ItemSpawner : MonoBehaviour
 {
@@ -7,23 +8,46 @@ public class ItemSpawner : MonoBehaviour
     public Transform[] spawnPoints;
     public int itemCount = 10;
 
+    private Queue<GameObject> itemPool = new Queue<GameObject>();
+
     void Start()
     {
+        InitializePool();
         SpawnItems();
+    }
+
+    void InitializePool()
+    {
+        for (int i = 0; i < itemCount; i++)
+        {
+            GameObject item = Instantiate(itemPrefab);
+            item.SetActive(false);
+            itemPool.Enqueue(item);
+        }
     }
 
     void SpawnItems()
     {
         for (int i = 0; i < itemCount; i++)
         {
-            Transform point = spawnPoints[Random.Range(0, spawnPoints.Length)];
-            Instantiate(itemPrefab, point.position, Quaternion.identity);
+            if (itemPool.Count > 0)
+            {
+                GameObject item = itemPool.Dequeue();
+                Transform point = spawnPoints[Random.Range(0, spawnPoints.Length)];
+                item.transform.position = point.position;
+                item.SetActive(true);
+            }
         }
 
-        GameManager.Instance.SetTargetCount(10);
+        GameManager.Instance.SetTargetCount(itemCount);
+    }
+
+    public void ReturnItemToPool(GameObject item)
+    {
+        item.SetActive(false);
+        itemPool.Enqueue(item);
     }
 }
-
 
 
 
